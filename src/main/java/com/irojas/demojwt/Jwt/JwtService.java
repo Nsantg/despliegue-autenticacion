f10a15bf5b69c143f87d9c56f43b6501bef2e776
Nsantg <nscarmona_170@cue.edu.co>
2025-03-25 08:33:48 -0500

    commit1

---
+++ src/main/java/com/irojas/demojwt/Jwt/JwtService.java
@@ -0,0 +1,78 @@
+package com.irojas.demojwt.Jwt;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import com.irojas.demojwt.User.User;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY="PmAi8x1QD5ewfwFrnx9IBaTw9MhZbTibJ0Hr1auTYXc=";
+
+    public String getToken(UserDetails user) {
+        Map<String, Object> claims = new HashMap<>();
+        if (user instanceof User) {
+            claims.put("role", ((User) user).getRole().name());
+        }
+        return getToken(claims, user);
+    }
+
+    private String getToken(Map<String,Object> extraClaims, UserDetails user) {
+        return Jwts
+                .builder()
+                .claims(extraClaims)
+                .subject(user.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getClaim(token, Claims::getSubject);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private Claims getAllClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T> T getClaim(String token, Function<Claims,T> claimsResolver) {
+        final Claims claims = getAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date getExpiration(String token) {
+        return getClaim(token, Claims::getExpiration);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return getExpiration(token).before(new Date());
+    }
+}
